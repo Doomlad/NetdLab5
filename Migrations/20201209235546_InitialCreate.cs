4c0dd6682ff5434987a791668a1b50c28d2b2dd8
Doomlad <coolbrampton@gmail.com>
2020-12-09 20:50:26 -0500

    main

---
+++ Migrations/20201209235546_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NetdLab5.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    carID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    make = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    model = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    year = table.Column<int>(type: "int", nullable: false),
+                    kilometers = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.carID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
---
+++ Models/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetdLab5.Models
+{
+    public class Car
+    {
+        public int carID { get; set; }
+
+        public string make { get; set; }
+
+        public string model { get; set; }
+
+        public int year { get; set; }
+
+        public int kilometers { get; set; }
+    }
+}
